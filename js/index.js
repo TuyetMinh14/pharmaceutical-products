919a97650951ba66c96acacb789f9d85e97b191a
TuyetMinh14 <Lucybieber2003@gmail.com>
2023-12-11 23:33:18 +0700

    update

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var slider_img = document.querySelector('.slider-1');
+    var images = ['slide1.jpeg', 'slide2.jpeg', 'slide3.jpeg'];
+    var i = 0;
+
+    function prev(){
+        if(i <= 0) i = images.length;	
+        i--;
+        return setImg();			 
+    }
+    
+    function next(){
+        if(i >= images.length-1) i = -1;
+        i++;
+        return setImg();			 
+    }
+    
+    function setImg(){
+        return slider_img.setAttribute('src', '/img/index/images/'+images[i]);
+        
+    }
+
+    document.querySelector('.prev').addEventListener('click', prev);
+    document.querySelector('.next').addEventListener('click', next);
+});
